342e96ed57425b94ddf72a1eb9a3ee09a60d7a7c
Jordan Halfman <jordanhalfman@gmail.com>
2022-09-02 08:17:07 -0700

    commit tree 1309e9ef7f3ce85af48981a893693563959b38ac

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    
+    def initialize
+        @knowledge = []
+    end
+    
+    def learn(new_stuff)
+        @knowledge << new_stuff
+    end
 
+    def knowledge
+        @knowledge
+    end
 end
